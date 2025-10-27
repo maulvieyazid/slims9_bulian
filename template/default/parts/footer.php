fea0bb581b8d77befe29b2dbbb196f3e9334bab0
maulvieyazid <17469172+maulvieyazid@users.noreply.github.com>
2025-10-27 11:37:06 +0800

    sembunyikan tombol donasi slims dan github slims, serta ganti nama developer nya

--- template/default/parts/footer.php
+++ template/default/parts/footer.php
@@ -67,17 +67,17 @@
                     </div>
                 </form>
                 <hr>
-                <a target="_blank" title="Support Us" class="btn btn-outline-success mb-2"
+                <!-- <a target="_blank" title="Support Us" class="btn btn-outline-success mb-2"
                    href="https://slims.web.id/web/pages/support-us/"><i
-                            class="fas fa-heart mr-2"></i><?= __('Keep SLiMS Alive'); ?></a>
-                <a target="_blank" title="Contribute" class="btn btn-outline-light mb-2"
+                            class="fas fa-heart mr-2"></i><?= __('Keep SLiMS Alive'); ?></a> -->
+                <!-- <a target="_blank" title="Contribute" class="btn btn-outline-light mb-2"
                    href="https://github.com/slims/"><i
-                            class="fab fa-github mr-2"></i><?= __('Want to Contribute?'); ?></a>
+                            class="fab fa-github mr-2"></i><?= __('Want to Contribute?'); ?></a> -->
             </div>
         </div>
         <hr>
         <div class="flex font-thin text-sm">
-            <p class="flex-1">&copy; <?php echo date('Y'); ?> &mdash; Senayan Developer Community</p>
+            <p class="flex-1">&copy; <?php //echo date('Y'); ?> 2025 &mdash; Tim Pustaka Nusantara</p>
             <div class="flex-1 text-right text-grey"><?= __('Powered by '); ?><code>SLiMS</code></div>
         </div>
     </div>
